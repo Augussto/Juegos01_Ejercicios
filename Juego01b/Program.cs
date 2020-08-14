dd5b6e8f6fab55022fb43922bf37460fb93cae21
Augussto <augusstocs@gmail.com>
2020-08-14 17:45:03 -0300

    Primer

---
+++ Juego01b/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Juego01b
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int miliseg = DateTime.Now.Millisecond;
+            int dado = miliseg % 100 + 1;
+            Console.WriteLine("El numero del dado es: "+dado);
+
+        }
+    }
+}
